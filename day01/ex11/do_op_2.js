16cb3a4ac11474ea322d28bb1b4f140d313242fc
Yacine Sibous <yacinesibous@gmail.com>
2018-06-29 22:12:24 -0400

    started ex11

--- day01/ex11/do_op_2.js
+++ day01/ex11/do_op_2.js
@@ -1,6 +1,13 @@
-if (process.argv.length == 5) {
+if (process.argv.length == 3) {
     process.argv.splice(0, 2);
-    var ops = process.argv;
+    var ops = process.argv[0];
+    ops = ops.split('*') || ops.split('+');
+    console.log(ops);
+    ops[0] = Number(ops[0]);
+    ops[1] = Number(ops[1]);
+    if (NaN(ops[0]) || NaN(ops[1])) {
+        console.log()
+    }
     if (ops[1] == '+') {
         console.log(Number(ops[0]) + Number(ops[2]));
     } else if (ops[1] == '-') {
@@ -11,6 +18,8 @@ if (process.argv.length == 5) {
         console.log(Number(ops[0]) / Number(ops[2]));
     } else if (ops[1] == '%') {
         console.log(Number(ops[0]) % Number(ops[2]));
+    } else {
+        console.log("Syntax Error.");
     }
 } else {
     console.log("Invalid number of arguments");
