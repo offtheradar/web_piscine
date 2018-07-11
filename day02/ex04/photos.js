7371423ff8443199e121a124987309f867376c81
Yacine Sibous <yacinesibous@gmail.com>
2018-07-11 12:44:10 -0400

    some

--- day02/ex04/photos.js
+++ day02/ex04/photos.js
@@ -1,18 +1,28 @@
-const scrape = require('website-scraper');
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const path = require('path');
-var path_name = process.argv[2];
-var options = {
-    uri: ['https://codeburst.io'],
-    transform: function (body) {
-        return cheerio.load(body);
-      }
-};
-rp(options)
-  .then(($) => {
-    console.log($);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+var request = require('request');
+var url = process.argv[2];
+const cheerio = require('cheerio')
+var content = undefined;
+request(url, (error, res, body) => {
+    if (!error) {
+        parse_html(body);
+    } else {
+        console.log(error);
+    }
+});
+
+function    parse_html(body) {
+    content += body;
+    const $ = cheerio.load(content);
+
+    var img_src = [];
+    $('img').each((i, elem) => {
+        console.log(elem.src);
+        img_src[i] = $(this).attr('src');
+    });
+    print_to_sc(img_src);
+    
+}
+
+function    print_to_sc(img) {
+    console.log(img);
+}
--- day04/ex00/index.js
+++ day04/ex00/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const port = 3000;
+const bodyParser = require('body-parser');
 const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.sendFile('/Users/yacinesibous/42Projects/web_piscine/day03/ex06/login.html');
+});
+app.get('/login', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.passwd;
+    if (req.body.submit == "ok") {
+        res.send("<html><body>OK</body></html>");
+    } else {
+        res.send("<html><body>Error</body></html>")
+    }
+});
+app.listen(8080);
